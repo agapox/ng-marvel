d43947884fd81acc01fac3f3a4de68dc0b5afe0f
jramirez <jose.ramirez@siigroup.cl>
2020-07-19 13:20:29 -0400

    refactor and hero details view

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { HeroModel } from 'src/app/models/hero.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  heroes: Array<HeroModel>;
+  totalHeroes: number = 0;
+
+  constructor(
+    private _heroeService: HeroesService,
+    private route: Router
+    ) { }
 
   ngOnInit() {
+    this._heroeService.getHeroes().subscribe(
+      (data: any) => {
+        console.log(data);
+        this.heroes = data.data.results;
+        this.createPagination(Number(data.data.total));
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
   }
 
+  showHeroeDetails(heroeId: number) {
+    console.log(heroeId);
+    this.route.navigate(['heroe', heroeId])
+  }
+
+  createPagination(totalHeroes: number) {
+    console.log('createPagination');
+    console.log(totalHeroes);
+    let pagination = Math.ceil(totalHeroes/this.heroes.length);
+    console.log('Pagination ', pagination);
+  }
 }
--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -3,11 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { HomeComponent } from './home/home.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  {
+    path: 'heroe', children: [
+      { path: ':heroId', component: HeroDetailsComponent }
+    ]
+  },
   { path: 'sandbox', component: SandboxComponent }
+
 ];
 
 @NgModule({
